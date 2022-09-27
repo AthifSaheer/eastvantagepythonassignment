310df36f6ac274fafae488f796d1337f18ab43bd
Athif Saheer <liteboook@gmail.com>
2022-09-27 18:44:16 +0530

    crud operation completed on addressbook

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_db():
     finally:
         db.close()
 
-geolocator = Nominatim(user_agent="MyApp")
+geolocator = Nominatim(user_agent="AddressBookApp")
 
 class AddressBook(BaseModel):
     first_name: str = Field(min_length=1)
@@ -28,7 +28,7 @@ class AddressBook(BaseModel):
     location: str = Field(min_length=1, max_length=100)
 
 
-ADDRESSBOOKS = []
+# ADDRESSBOOKS = []
 
 
 @app.get("/")
@@ -46,15 +46,8 @@ def create_address(address: AddressBook, db: Session = Depends(get_db)):
     address_model.location = address.location
 
     location = geolocator.geocode(address_model.location)
-
-    # location = lag_log(address_model.location)
-
-    # location = lag_log(address_model.location)
-    # asyncio.sleep(1)
-
     address_model.latitude = location.latitude
     address_model.longitude = location.longitude
-
 
     db.add(address_model)
     db.commit()
@@ -62,39 +55,43 @@ def create_address(address: AddressBook, db: Session = Depends(get_db)):
     return address
 
 
-# @app.put("/{book_id}")
-# def update_book(book_id: int, book: AddressBook, db: Session = Depends(get_db)):
+@app.put("/{address_id}")
+def update_address(address_id: int, address: AddressBook, db: Session = Depends(get_db)):
 
-#     book_model = db.query(models.AddressBook).filter(models.AddressBook.id == book_id).first()
+    address_model = db.query(models.AddressBook).filter(models.AddressBook.id == address_id).first()
 
-#     if book_model is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail=f"ID {book_id} : Does not exist"
-#         )
+    if address_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {address_id} : Does not exist"
+        )
 
-#     book_model.title = book.title
-#     book_model.author = book.author
-#     book_model.description = book.description
-#     book_model.rating = book.rating
+    address_model.first_name = address.first_name
+    address_model.last_name = address.last_name
+    address_model.postcode = address.postcode
+    address_model.location = address.location
 
-#     db.add(book_model)
-#     db.commit()
+    location = geolocator.geocode(address_model.location)
+    address_model.latitude = location.latitude
+    address_model.longitude = location.longitude
 
-#     return book
+    db.add(address_model)
+    db.commit()
+
+    return address
 
 
-# @app.delete("/{book_id}")
-# async def delete_book(book_id: int, db: Session = Depends(get_db)):
+@app.delete("/{address_id}")
+async def delete_address(address_id: int, db: Session = Depends(get_db)):
 
-#     book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
+    address_model = db.query(models.AddressBook).filter(models.AddressBook.id == address_id).first()
 
-#     if book_model is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail=f"ID {book_id} : Does not exist"
-#         )
+    if address_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {address_id} : Does not exist"
+        )
 
-#     db.query(models.Books).filter(models.Books.id == book_id).delete()
+    db.query(models.AddressBook).filter(models.AddressBook.id == address_id).delete()
 
-#     db.commit()
+    db.commit()
